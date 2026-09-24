739942c2d2ba6e7cc751ff100ee69399313ed92c
Mohammad Aayan <70325166+aayan398@users.noreply.github.com>
2026-09-24 21:15:26 +0100

    add python website link

--- hello.py
+++ hello.py
@@ -15,6 +15,7 @@ def say_hello():
 @app.route('/about')
 def about():
     return ('<p>This application is running on the Flask web framework.</p>'
+           ' <p><a href="https://www.python.org/">Python website</a></p>'
             '<p><a href="https://flask.palletsprojects.com/">Visit the Flask website</a></p>'
             '<p><a href="/">Back to home</a></p>')
 
